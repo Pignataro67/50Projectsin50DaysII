14c1916c54603c46541422c7a8c9620e69f00912
Pignataro67 <Pignataro67@gmail.com>
2021-01-04 08:09:39 -0800

    adding a function

--- script.js
+++ script.js
@@ -1,62 +1,30 @@
 const smallCups = document.querySelectorAll('.cup-small')
 const listers = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
-const remained = document.getElementById('remained')
-
-updateBigCup()
+const remained = document.getElementById('remaineed')
 
 smallCups.forEach((cup, idx) => {
-    cup.addEventListener('click', () => highlightCups(idx))
+  cup.addEventListener('click', () => highlightCups(idx))  
 })
 
 function highlightCups(idx) {
-  if (idx===7 && smallCups[idx].classList.contains("full")) idx--;
   if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
-    idx--
+    idx-- 
   }
-
+  
   smallCups.forEach((cup, idx2) => {
     if(idx2 <= idx) {
       cup.classList.add('full')
     } else {
-      cup.classList.remove('full')
-      }
+      cup.classList.remove('full')    
+    }
   })
-
-  updateBigCup()
-}
-
-function updateBigCup() {
-    const fullCups = document.querySelectorAll('.cup-small.full').length
-    const totalCups = smallCups.length
-
-    if(fullCups === 0) {
-        percentage.style.visibility = 'hidden'
-        percentage.style.height = 0
-    } else {
-        percentage.style.visibility = 'visible'
-        percentage.style.height = `${fullCups / totalCups * 330}px`
-        percentage.innerText = `${fullCups / totalCups * 100}%`
-    }
-
-    if(fullCups === totalCups) {
-        remained.style.visibility = 'hidden'
-        remained.style.height = 0
-    } else {
-        remained.style.visibility = 'visible'
-        listers.innerText = `${2 - (250 * fullCups / 1000)}L`
-    }
 }
 
 // const toggle = document.getElementById('toggle');
 // const nav = document.getElementById('nav');
 
 // toggle.addEventListener('click', () => nav.classList.toggle('active'))
-
-
-
-
-
 
 
 // const tagsEl =document.getElementById('tags');
